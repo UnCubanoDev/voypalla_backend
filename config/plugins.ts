70a550c5c60735fefb19535f26025d178b0afbc7
UnCubanoDev <rnaranjoalbert@gmail.com>
2024-12-25 16:46:04 -0500

    Update production deployment script, enhance API documentation, and refine JSON schema definitions
    - Changed directory in Production.yml to point to the backend folder and updated npm commands to use pnpm.
    - Enhanced API documentation in plugins.ts by adding detailed metadata including contact information and server URLs.
    - Updated full_documentation.json to reflect changes in contact details and server configurations.
    - Standardized tags in JSON schema definitions across multiple endpoints for consistency.

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,40 @@
-export default () => ({});
+export default () => ({
+  documentation: {
+    enabled: true,
+    config: {
+      openapi: '3.0.0',
+      info: {
+        version: '1.0.0',
+        title: 'VoyPaLlá API',
+        description: '',
+        termsOfService: '',
+        contact: {
+          name: 'VoyPaLlá Team',
+          email: 'voypalla.app@gmail.com',
+          url: 'voypalla.uncubano.dev',
+        },
+        license: {
+          name: 'Apache 2.0',
+          url: 'https://www.apache.org/licenses/LICENSE-2.0.html',
+        },
+      },
+      'x-strapi-config': {
+        // Leave empty to ignore plugins during generation
+        plugins: ['upload', 'users-permissions'],
+        path: '/documentation',
+      },
+      servers: [
+        { url: 'http://localhost:8082/api', description: 'Development server' },
+        {
+          url: 'https://voypalla.uncubano.dev/api',
+          description: 'Production server',
+        },
+      ],
+      externalDocs: {
+        description: 'Find out more',
+        url: 'https://docs.strapi.io/developer-docs/latest/getting-started/introduction.html',
+      },
+      security: [{ bearerAuth: [] }],
+    },
+  },
+});
